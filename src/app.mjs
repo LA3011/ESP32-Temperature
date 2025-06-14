fe3eb7db401732b7fb401048d0c06b84fc2eac10
Luis A <159967782+LA3011@users.noreply.github.com>
2025-06-14 18:54:17 -0400

    Update app.mjs

--- src/app.mjs
+++ src/app.mjs
@@ -248,9 +248,12 @@ async function verificarEstadoESP32() {
 
 
 // Programar verificación periódica (cada 10 minutos por defecto)
-const cronTime = "*/5 * * * *";
-// const cronTime = process.env.CRON_TIME ||  "*/5 * * * *";
-cron.schedule(cronTime, verificarEstadoESP32);
+// const cronTime = "*/5 * * * *";
+// cron.schedule(cronTime, verificarEstadoESP32);
+const cronTime = process.env.CRON_TIME ||  "*/5 * * * *";
+cron.schedule(cronTime, () => {
+  verificarEstadoESP32();
+});
 
 // ─────────────────────────────────────────────
 // Cron job para reiniciar el contador diario a medianoche
